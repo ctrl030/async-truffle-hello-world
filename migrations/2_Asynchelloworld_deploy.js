7d6b9b4866fce2e34bc0c4464fad7e7aa3adb0eb
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2020-11-12 17:32:32 -0300

    8

--- migrations/2_Asynchelloworld_deploy.js
+++ migrations/2_Asynchelloworld_deploy.js
@@ -22,30 +22,14 @@ module.exports = async function(deployer, network, accounts) {
     const instance = await AsyncHelloWorld.deployed();
     
     console.log("Me: let instance = await deployer.deploy(AsyncHelloWorld) was run.");
-
-    try {
-      await instance.setMessage("Set Message was called and worked.", {value: 100000000000000000, from: accounts[0]});
-    }
-
-    catch (err) {
-      console.log("Error during setting Message: " + err)
-    }
+    
+    await instance.setMessage("Set Message was called and worked.", {value: 100000000000000000, from: accounts[0]});
+    
+    console.log("Success");
   }
 
   catch (err) {
-    console.log("Error during whole deployment: " + err)
-  }
-
-  console.log("Success");
-
-  /* 
-    }).catch(function(err){
-      console.log("this went wrong during setMessage: " + err);
-    }); 
-  }).catch(function(err){
-    console.log("this went wrong during deployment: " + err); 
-  });  */
+    console.log("Error: " + err)
+  }  
 
 };
-
-// test
