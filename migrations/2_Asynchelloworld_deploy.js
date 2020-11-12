6c659da5ab8295110baa0bf3b885cf4844d0ea7a
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2020-11-11 21:47:10 -0300

    5

--- migrations/2_Asynchelloworld_deploy.js
+++ migrations/2_Asynchelloworld_deploy.js
@@ -1,8 +1,27 @@
 const AsyncHelloWorld = artifacts.require("AsyncHelloWorld");
 
-module.exports = async function(deployer, network, accounts) {
+/* module.exports = async function(deployer, network, accounts) {
   deployer.deploy(AsyncHelloWorld).then(function(instance){
-    instance.setMessage("Hey there!", {value: 100000000000000000, from: accounts[0]}).then(function(){
+    instance.setMessage("Set Message was called and worked.", {value: 100000000000000000, from: accounts[0]}).then(function(){
+      console.log("Success");
+    }).catch(function(err){
+      console.log("this went wrong during setMessage: " + err);
+    }); 
+  }).catch(function(err){
+    console.log("this went wrong during deployment: " + err); 
+  }); 
+}; */
+
+
+module.exports = async function(deployer, network, accounts) {
+
+  let instance = await deployer.deploy(AsyncHelloWorld) 
+  
+  console.log("Me: let instance = await deployer.deploy(AsyncHelloWorld) was run.")
+  .then(async function(instance){
+    instance.setMessage("Set Message was called and worked.", {value: 100000000000000000, from: accounts[0]})
+    
+  .then(async function(){
       console.log("Success");
     }).catch(function(err){
       console.log("this went wrong during setMessage: " + err);
@@ -11,6 +30,5 @@ module.exports = async function(deployer, network, accounts) {
     console.log("this went wrong during deployment: " + err); 
   }); 
 };
-
 
 // test
